5cc8193405ed689d393969e5a01ac7e53ea0eb60
Kynan <k.song@blueyonder.co.uk>
2019-01-18 15:01:54 +0000

    Can fetch openweather data

--- UdacityCocktailApp/View Controllers/GoingOutViewController.swift
+++ UdacityCocktailApp/View Controllers/GoingOutViewController.swift
@@ -16,30 +16,51 @@ class GoingOutViewController: UIViewController, CLLocationManagerDelegate {
     
     var locationManager: CLLocationManager = CLLocationManager()
     var userLocation: CLLocation!
+    var currentWeather: String = ""
+    var apiKey = "7f5cf61e2a9e2510653f76d1061015b6"
+    let openWeatherMapBaseURL = "http://api.openweathermap.org/data/2.5/weather"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getWeatherData(urlString: "api.openweathermap.org/data/2.5/weather?q=Edinburgh")
-    }
-
-    func getWeatherData(urlString: String) {
-        let url = URL(string: urlString)
-        let request = URLRequest(url: url!)
-        
-        let task = URLSession.shared.dataTask(with: request) {
-            (data, response, error) in
-            
-//            DispatchQueue.async(DispatchQueue.main(), {
-//                self.setLabel(weatherData: data as! NSData)
-//            })
-            
-        }
+        getWeatherData(city: "Edinburgh")
         
     }
     
-    func setLabel(weatherData: NSData) {
-        
+    override func viewWillAppear(_ animated: Bool) {
+        decisionLabel.text = currentWeather
     }
+    
+    
+    func getWeatherData(city: String) {
+        let session = URLSession.shared
+        let weatherRequest = URL(string: "\(openWeatherMapBaseURL)?APPID=\(apiKey)&q=\(city)")
+        
+        let dataTask = session.dataTask(with: weatherRequest!) {
+            (data, response, error) in
+            
+            if let error = error {
+                print("\(error)")
+            }
+            else {
+                let dataString = String(data: data!, encoding: String.Encoding.utf8)
+                print("The weather in Edinburgh is: \(data) + \(dataString)")
+                
+                do {
+                    let weather: [String : AnyObject] = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
+                    //self.currentWeather = "Weather data for \(weather["name"]!)"
+                    print("Weather data for \((weather["weather"]![0]! as! [String: AnyObject])["main"]!)")
+                }
+                catch let jsonError as NSError {
+                    print("JSON error: \(jsonError.description)")
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    
+
+    
 
 }
